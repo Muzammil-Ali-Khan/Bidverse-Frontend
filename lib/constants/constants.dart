0fea5bee3ab44a6ac29a67635d2b448937c99163
Muzammil-Ali-Khan <muzammilali1512@gmail.com>
2023-06-03 09:35:25 +0500

    feat: Splash Screen added

---
+++ lib/constants/constants.dart
@@ -0,0 +1,8 @@
+import 'dart:ui';
+
+// Colors
+const primaryColor = Color(0xffFC9905);
+const darkBlueColor = Color(0xff24283B);
+
+// Images
+const logoWithoutBg = "assets/images/logoWithoutBg.png";
--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -1,3 +1,4 @@
+import 'package:bidverse_frontend/constants/constants.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
@@ -8,7 +9,18 @@ class SplashScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        body: Text(AppLocalizations.of(context)!.helloWorld),
+        backgroundColor: darkBlueColor,
+        body: Center(
+          child: Container(
+            height: MediaQuery.of(context).size.height * 0.3,
+            width: MediaQuery.of(context).size.width * 0.8,
+            decoration: BoxDecoration(
+              color: primaryColor,
+              borderRadius: BorderRadius.circular(20.0),
+            ),
+            child: Image.asset(logoWithoutBg),
+          ),
+        ),
       ),
     );
   }
